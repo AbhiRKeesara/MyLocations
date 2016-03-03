57d1e3fbc6af6d10aa0bc7358d6dacda444b771c
AbhiRKeesara <keesaraabhinay@gmail.com>
2016-03-02 20:23:29 -0500

    Initial Commit

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by abhinay reddy keesara on 3/2/16.
+//  Copyright © 2016 abhinay reddy keesara. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
